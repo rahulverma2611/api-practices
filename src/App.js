e3273d37095e4035825f14882ae44bca496a031b
rahul <rv22090@gmail.com>
2024-06-25 10:26:25 +0530

    fgfbd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    remove();
+  }, []);
+
+  function remove() {
+    fetch("http://localhost:3000/users").then((response) => {
+      response.json().then((result) => {
+        setUser(result);
+      });
+    });
+  }
+
+  function deleteUser(id) {
+    // fetch(`http://localhost:3000/users/${id}`, {
+    //   method: "delete",
+    // }).then((deleted) => {
+    //   deleted.json().then((resp) => {
+    //     console.log(resp);
+    //     remove();
+    //   });
+    // });
+    axios
+      .delete(`http://localhost:3000/users/${id}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((deleteUser) => {
+        console.log("deleteUser", deleteUser);
+        remove();
+      });
+  }
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  function addUser() {
+    // console.log("form", name, email, phone);
+    const userDetail = { name, email, phone };
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userDetail),
+    }).then((response) => {
+      response.json().then((finalDetail) => {
+        console.log(finalDetail);
+      });
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>delete api practice</h2>
+      </div>
+
+      {/* -----------------get api method starts------------- */}
+      <table border="1">
+        <tr>
+          <th>name</th>
+          <th>email</th>
+          <th>phone</th>
+        </tr>
+        {user.map((item) => {
+          return (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+              <td>
+                <button onClick={() => deleteUser(item.id)}>delete</button>
+              </td>
+            </tr>
+          );
+        })}
+      </table>{" "}
+      <br />
+      <br />
+      <br />
+      <br />
+      {/* --------------------------------- post api method starts-----------------------------*/}
+      <div>
+        <input
+        placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="name"
+        />{" "}
+        <br />
+        <br />
+        <input
+        placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+        />
+        <br />
+        <br />
+        <input
+        placeholder="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          type="phone"
+        />
+        <br />
+        <br />
+        <button type="submit" onClick={addUser}>
+          submit
+        </button>
+      </div>
     </div>
   );
 }
